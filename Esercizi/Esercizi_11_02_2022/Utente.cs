64754a3a4f5559694ca2398a79e1b41274581484
Luca <lucacavallaro504@gmail.com>
2022-02-11 16:02:44 +0100

    Esercizi

--- Esercizi/Esercizi_11_02_2022/Utente.cs
+++ Esercizi/Esercizi_11_02_2022/Utente.cs
@@ -2,5 +2,14 @@ using System;
 
 
 public class Utente{
-    
+    private string id;
+    private string nome;
+    private string cognome;
+
+    private int annoIscrizione;
+    public Utente()
+    {
+
+    }
+
 }
